f88a31621ad56efd3c16412533145cc8aead4817
Dimitris-Kaler <74980408+Dimitris-Kaler@users.noreply.github.com>
2021-08-26 19:11:02 +0300

    nav for birds and fish

---
+++ mvc/assets/js/navbar.js
@@ -0,0 +1,21 @@
+var nav = document.getElementById('navbar');
+    var list = document.querySelectorAll('a');
+    
+
+    window.onscroll = function(){
+        if(window.pageYOffset > 100) {
+            // nav.style.background =  "transparent";
+            nav.style.background =  "rgba(255, 249, 192, 0.7)";
+            list.forEach(element =>{
+                element.style.color =  "green";
+                
+            })
+            
+        }else{
+            nav.style.background =  "green";
+            list.forEach(element =>{
+                element.style.color =  "lightgrey";
+                
+            })
+        }
+    }
