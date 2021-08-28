561b45369d04d52979b15bf24fd7f6e0aca220cd
B7AST <85998893+B7AST@users.noreply.github.com>
2021-08-28 16:41:02 +0300

    modifications28/8/21
    ejs & navbar modifications

--- mvc/assets/js/navbar.js
+++ mvc/assets/js/navbar.js
@@ -6,6 +6,7 @@ var nav = document.getElementById('navbar');
         if(window.pageYOffset > 100) {
             // nav.style.background =  "transparent";
             nav.style.background =  "rgba(255, 249, 192, 0.7)";
+            nav.style.height =  "3rem";
             list.forEach(element =>{
                 element.style.color =  "green";
                 
@@ -13,6 +14,7 @@ var nav = document.getElementById('navbar');
             
         }else{
             nav.style.background =  "green";
+            nav.style.height =  "6.9rem";
             list.forEach(element =>{
                 element.style.color =  "lightgrey";
                 
