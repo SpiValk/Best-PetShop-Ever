738d58a1adb945e936251815031de99cc2f0696d
Dimitris-Kaler <74980408+Dimitris-Kaler@users.noreply.github.com>
2021-08-31 16:22:12 +0300

    sxolia
    ger

--- mvc/assets/js/navbar.js
+++ mvc/assets/js/navbar.js
@@ -1,27 +1,29 @@
+
+//DECLARE VARIABLES
+
 var nav = document.getElementById('navbar');
-    var list = nav.querySelectorAll('a');
-    
+var list = nav.querySelectorAll('a');
 
-    window.onscroll = function(){
-        if(window.pageYOffset > 100) {
-            // nav.style.background =  "transparent";
-            nav.style.background =  "rgba(255, 249, 192, 0.7)";
-            nav.style.height =  "3rem";
-            list.forEach(element =>{
-                element.style.color =  "green";
-                
-            })
-            
-        }else{
-            nav.style.background =  "green";
-            nav.style.height =  "6.9rem";
-            list.forEach(element =>{
-                element.style.color =  "lightgrey";
-                
-            })
-        }
+
+window.onscroll = function () {
+    if (window.pageYOffset > 100) {
+        // nav.style.background =  "transparent";
+        nav.style.background = "rgba(255, 249, 192, 0.7)";
+        nav.style.height = "3rem";
+        list.forEach(element => {
+            element.style.color = "green";
+
+        })
+
+    } else {
+        nav.style.background = "green";
+        nav.style.height = "6.9rem";
+        list.forEach(element => {
+            element.style.color = "lightgrey";
+
+        })
     }
+}
 
 
 
-        
