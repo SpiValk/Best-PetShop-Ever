46c183405bb738b9c999839b623f825ee65e4625
KostasRokkas <79996907+KostasRokkas@users.noreply.github.com>
2021-09-06 01:03:11 +0300

    CORRECT login part a!!!!!
    ayto einai to swsto meta apo kapoies diorthwseis

--- mvc/api/controllers/users/checklogin.js
+++ mvc/api/controllers/users/checklogin.js
@@ -9,7 +9,6 @@ module.exports = {
   
     fn: async function({username, password}){
         if (username == 'admin' && password == 'admin') {
-            console.log(this.req.session)
             this.req.session.cookie.maxAge = sails.config.custom.rememberMeCookieMaxAge
             this.req.session.username = username    // <-- login
             // if (sails.hooks.sockets) {
