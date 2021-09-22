9106873fe717265b3da591e7d0c33572e7b46b67
SpiValk <spiros.valkaniotis@gmail.com>
2021-09-22 13:50:47 +0300

    Merged user and customer together

--- mvc/api/models/User.js
+++ mvc/api/models/User.js
@@ -2,37 +2,17 @@ module.exports={
   tableName:'user',
   attributes:{
 
-    username :{
-      type:'string',
-      columnName:'username',
-      required:true,
-      unique: true,
-    },
+    username        : { type:'string', columnName:'username', required:true, unique: true,},
+    password        : { type:'string', columnName:'password', required:true, protect: true},
+    email           : { type:'string', isEmail: true, columnName:'email', required:true,unique: true},
+    isAdmin         : { type:'number', columnName:'isAdmin', required:true},
 
-    password :{
-      type:'string',
-      columnName:'password',
-      required:true,
-      protect: true},
-
-    email :{
-      type:'string',
-      isEmail: true,
-      columnName:'email',
-      required:true,
-      unique: true},
+    firstName       : { type:'string', columnName:'first_name',     required:true },
+    lastName        : { type:'string', columnName:'last_name',      required:true },
+    address         : { type:'string', columnName:'address',        required:true },
+    contact_number  : { type:'number', columnName:'contact_number', required:true },
 
 
-    isAdmin :{
-      type:'number',
-      columnName:'isAdmin',
-      required:true},
-
-
-    customer_id:{
-      collection: 'customer',
-      via: 'user_id'
-    },
 
     order_id:{
       model:'order'
