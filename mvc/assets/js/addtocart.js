9ba4bc7af31e69821fb69cb370dfdca9bc4c5ada
Dimitris-Kaler <74980408+Dimitris-Kaler@users.noreply.github.com>
2021-09-24 03:27:07 +0300

    cart view local storage

--- mvc/assets/js/addtocart.js
+++ mvc/assets/js/addtocart.js
@@ -1,12 +1,12 @@
 
-var Itemonly = localStorage.getItem('shoppingCart');
+var Items = localStorage.getItem('shoppingCart');
 // console.log(Itemonly)
 // console.log(typeof Itemonly)
 let productsCartView = document.getElementById("display-product-in-cart-view")
-let finalOrderPrice=document.getElementById("check-amt")
-let removeProductBtns=document.getElementsByClassName("cart-product-remove")
-let cartProductBox=document.getElementsByClassName("cartViewProduct")
-let cartProductId=document.getElementsByClassName("id")
+let finalOrderPrice = document.getElementById("check-amt")
+let removeProductBtns = document.getElementsByClassName("cart-product-remove")
+let cartProductBox = document.getElementsByClassName("cartViewProduct")
+let cartProductId = document.getElementsByClassName("id")
 // let parentElement = document.querySelector('#buyItems');
 // let cartSumPrice = document.querySelector('#sum-prices');
 // console.log(cartSumPrice1)
@@ -17,28 +17,23 @@ let cartProductId=document.getElementsByClassName("id")
 // setTimeout(function(){ console.log(cartProductId.length) }, 0);
 // console.log(Object.keys(cartProductId))
 
-var pull = JSON.parse(Itemonly)
+var pull = JSON.parse(Items)
 const countSumPrice1 = function () { // 4
     let sum = 0;
     pull.forEach(item => {
-      sum += item.price;
+        sum += item.price;
     });
     return sum;
-  };
+};
 
 
 
 
-console.log(pull)
-console.log(localStorage)
-console.log(Itemonly)
 if (pull.length > 0) {
     let result = pull.map(carprod => {
-        return ` <div class="row d-flex justify-content-center border-top cartViewProduct">
-                   
+        return ` <div class="row d-flex justify-content-center border-top cartViewProduct">         
         <div class="col-5">
             <div class="row d-flex">
-            <p hidden><span class="id">${carprod.id}</span></p>
                 <div class="book"> <img src="${carprod.image}" class="book-img"> </div>
                 <div class="my-auto flex-column d-flex pad-left">
                     <h6 class="mob-text">${carprod.name}</h6>
@@ -57,7 +52,7 @@ if (pull.length > 0) {
                     <h6 class="mob-text">$${carprod.price}</h6>
                 </div>
                 <div class="col-4">
-                    <button class="btn btn-danger cart-product-remove">Remove</button>
+                    <button class="btn btn-danger cart-product-remove" id="${carprod.id}">Remove</button>
                 </div>
             </div>
         </div>
@@ -66,36 +61,40 @@ if (pull.length > 0) {
     )
     productsCartView.innerHTML = result
 }
-else{
-    productsCartView.innerHTML="Your shopping cart is empty"
+else {
+    productsCartView.innerHTML = "Your shopping cart is empty"
 }
 
 
 
-finalOrderPrice.innerHTML="Checkout $"+countSumPrice1()
+finalOrderPrice.innerHTML = "Checkout $" + countSumPrice1()
+
+
+// <p hidden><span class="id">${carprod.id}</span></p>
 
 
 //REMOVE ELEMENTS FROM CART VIEW
 
-function localremove(){ 
-    
-    for (let i=0;i <pull.length; i++){
-        // setTimeout(function(){console.log(pull[i])},1000)
-        // setTimeout(function(){console.log(pull)},1000)
-        console.log(pull.length)
-        for(let viewProduct of cartProductId){
-            // console.log(cartProductId)
-            // console.log(pull[i].id!==viewProduct.innerHTML)
-        if(pull[i].id===viewProduct.innerHTML){
-            console.log(pull[i].id===id)
-      pull.splice(i,1)
-      localStorage.setItem('shoppingCart', JSON.stringify(pull));}}
-          console.log(pull)
-          
-          
-          console.log(localStorage)
-            if (pull.length > 0) {
-              let result = pull.map(product => {
+function localremove(e) {
+
+
+    for (let i = 0; i < pull.length; i++) {
+
+
+        if (pull[i].id == e.target.id) {
+            // setTimeout(function(){console.log(pull[i].id)},0)
+
+         pull.splice(i, 1)
+            console.log(pull)
+
+        }
+
+        //   console.log(newPull)
+
+        localStorage.setItem('shoppingCart', JSON.stringify(pull));
+
+        if (pull.length > 0) {
+            let result = pull.map(product => {
                 return `
                           <li class="buyItem">
                               <img class="sideCartImg" src="${product.image}">
@@ -109,54 +108,85 @@ function localremove(){
                                   </div>
                               </div>
                           </li>`;
-              });
-              parentElement.innerHTML = result.join('');
-              document.querySelector('.checkout').classList.remove('hidden');
-              cartSumPrice.innerHTML = '$' + countSumPrice1();
-          
-            }
-            else {
-              document.querySelector('.checkout').classList.add('hidden');
-              parentElement.innerHTML = '<h4 class="empty">Your shopping cart is empty</h4>';
-              cartSumPrice.innerHTML = '';
-            }
+            });
+            parentElement.innerHTML = result.join('');
+            document.querySelector('.checkout').classList.remove('hidden');
+            cartSumPrice.innerHTML = '$' + countSumPrice1();
 
-        }}
+        }
+        else {
+            document.querySelector('.checkout').classList.add('hidden');
+            parentElement.innerHTML = '<h4 class="empty">Your shopping cart is empty</h4>';
+            cartSumPrice.innerHTML = '';
+        }
 
-// function localremove(){
-//     let pullProducts
-//     for(let viewProduct of cartProductId){
-//         console.log(pull)
-//     pullProducts=pull.filter(product=>console.log(product.id!==viewProduct.innerHTML)
-//     )
+    }
+}
 
-    
-//     }
+//  function localremove(e){
+//     // let pullProducts=[]
+//         // console.log(event.target.id)
+
+//     let pullProducts=pull.filter(product=>product.id!==e.target.id)
+
+//     console.log(pullProducts)
 //     localStorage.setItem('shoppingCart', JSON.stringify(pullProducts))
-  
-// // console.log(pullProducts)
-// console.log(localStorage)
+//     // console.log( localStorage.getItem('shoppingCart'))
+//             if (pullProducts.length > 0) {
+
+//               let result = pullProducts.map(product => {
+//                 //   console.log(product)
+//                 return `
+//                           <li class="buyItem">
+//                               <img class="sideCartImg" src="${product.image}">
+//                               <div class="mainBodySideCart">
+//                                   <h5 class="sideCartName">${product.name}</h5>
+//                                   <h6>€ ${product.price}</h6>
+//                                   <div class="sideCartBtns">
+//                                       <button class="button-minus sideCountBtn" data-id=${product.id}>-</button>
+//                                       <span class="countOfProduct">${product.count}</span>
+//                                       <button class="button-plus sideCountBtn" data-id=${product.id}>+</button>
+//                                   </div>
+//                               </div>
+//                           </li>`;
+//               });
+//               parentElement.innerHTML = result.join('');
+//               console.log(parentElement)
+//               document.querySelector('.checkout').classList.remove('hidden');
+//               cartSumPrice.innerHTML = '$' + countSumPrice1();
+
+//             }
+//             else {
+//               document.querySelector('.checkout').classList.add('hidden');
+//               parentElement.innerHTML = '<h4 class="empty">Your shopping cart is empty</h4>';
+//               cartSumPrice.innerHTML = '';
+//             }}
+
+
+// console.log(pullProducts)
 // console.log(pull)}
 
 
 
-function removeElement(event){
-    
-    for(let box of cartProductBox){
-        if(box.contains(event.target)){
-            box.remove()
-           localremove()
+function removeElement(event) {
 
-        }}
-    
-  }
+    for (let box of cartProductBox) {
+        if (box.contains(event.target)) {
+            localremove(event)
+            box.remove()
+
+
+        }
+    }
+
+}
 
 // function removeElement(event){
 //     for(let box of cartProductBox){
 //                 if(box.contains(event.target)){
 //                     box.remove()
-                   
-        
+
+
 //                 }}
 
 //                 // let storageProducts = JSON.parse(localStorage.getItem('products'));
@@ -165,14 +195,14 @@ function removeElement(event){
 //             }    
 
 // }
-          
-    
-
-    
 
 
 
-for (let btn of removeProductBtns){
-    btn.addEventListener("click",removeElement)
+
+
+
+
+for (let btn of removeProductBtns) {
+    btn.addEventListener("click", removeElement)
 }
 
